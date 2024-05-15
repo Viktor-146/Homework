ba40d2f387d83bb11b52cd9640ee829e2a223a18
Viktor <Viktor@email.ru>
2024-05-15 10:23:05 +0300

    Добавлена папка с кодом

---
+++ Homework/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] array = { "123", "1567", "-2", "computer science", "22" };
+
+    string[] newArray = new string[array.Length];
+    int count = 0;
+
+    Console.WriteLine("Новый массив:");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            newArray[count] = array[i];
+            
+            count++;
+        }
+    }
+
+    string[] finalArray = new string[count];
+
+    for (int i = 0; i < count; i++)
+    {
+        finalArray[i] = newArray[i];
+        Console.Write(finalArray[i]);
+        if (i != count - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+Console.WriteLine();
